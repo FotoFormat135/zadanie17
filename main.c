96333ced695cd01a8bb8b2e1f6ec6a99650b8eb2
Mateusz Baczkowski <mateusz.baczkowski2000@gmail.com>
2025-01-10 11:19:39 +0100

    Rev.2

--- main.c
+++ main.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 
+float cube(){
+	float a,b,c;
+
+	printf("Podaj dlugosc boku A: ");
+	scanf("%f",&a);
+	printf("Podaj dlugosc boku B: ");
+	scanf("%f",&b);
+	printf("Podaj dlugosc boku C: ");
+	scanf("%f",&c);
+
+	return a*b*c;
+}
+
+float sphere(){
+	float r;
+
+	printf("Podaj pronien R: ");
+	scanf("%f",&r);
+
+	return 1.33 * 3.14 * r*r*r;
+}
+
 int main(){
+	int w;
 
-	int a,b,c;
+	ptintf("Oblicz objetosc\n-----------\n1.Prostopadloscianu\n2.Kuli\n:");
+	scanf("%d", &w);
 
-	printf("Podej dlugosc boku A: ");
-	scanf("%d",&a);
-	printf("Podej dlugosc boku B: ");
-	scanf("%d",&b);
-	printf("Podej dlugosc boku C: ");
-	scanf("%d",&c);
+	switch(w){
+		case 1:
+			printf("\nA x B x C = %f", cube());
+		break;
+		case 2:
+			printf("\n4/3 x PI x R^3 = %f", sphere());
+		break;
+	}
 
-	int wynik = a*b*c;
-
-	printf("\nObjetosc prostopadloscianu\nA x B x C = %d", wynik);
+return 0;
 }
